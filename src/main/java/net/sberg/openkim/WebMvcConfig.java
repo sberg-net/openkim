4d17c2cc773ffe13c3569c406d6548d6a2ef305c
stephan <st.schoenberg@sberg.net>
2023-01-12 15:23:57 +0100

    clean up

--- src/main/java/net/sberg/openkim/WebMvcConfig.java
+++ src/main/java/net/sberg/openkim/WebMvcConfig.java
@@ -16,21 +16,18 @@
  */
 package net.sberg.openkim;
 
-import org.springframework.beans.BeansException;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Description;
-import org.springframework.context.support.ResourceBundleMessageSource;
-import org.springframework.format.FormatterRegistry;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
 import org.thymeleaf.extras.springsecurity6.dialect.SpringSecurityDialect;
 import org.thymeleaf.spring6.SpringTemplateEngine;
@@ -46,16 +43,6 @@ import java.util.List;
 @ComponentScan
 public class WebMvcConfig implements WebMvcConfigurer {
 
-    /*private ApplicationContext applicationContext;
-
-    public WebMvcConfig() {
-        super();
-    }
-
-    public void setApplicationContext(final ApplicationContext applicationContext) throws BeansException {
-        this.applicationContext = applicationContext;
-    }*/
-
     @Bean(name = "multipartResolver")
     public StandardServletMultipartResolver multipartResolver() {
         StandardServletMultipartResolver multipartResolver = new StandardServletMultipartResolver();
@@ -70,53 +57,17 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
   @Bean
     public SpringResourceTemplateResolver templateResolver(){
-        // SpringResourceTemplateResolver automatically integrates with Spring's own
-        // resource resolution infrastructure, which is highly recommended.
         SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
-        //templateResolver.setApplicationContext(this.applicationContext);
         templateResolver.setPrefix("classpath:/templates/");
         templateResolver.setSuffix(".html");
-        // HTML is the default value, added here for the sake of clarity.
         templateResolver.setTemplateMode(TemplateMode.HTML);
         templateResolver.setCharacterEncoding("UTF-8");
-        // Template cache is true by default. Set to false if you want
-        // templates to be automatically updated when modified.
         templateResolver.setCacheable(false);
         return templateResolver;
     }
 
-    /*@Bean
-    @Description("Thymeleaf template resolver serving HTML 5")
-    public ClassLoaderTemplateResolver templateResolver() {
-
-        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
-
-        templateResolver.setPrefix("templates/");
-        templateResolver.setCacheable(false);
-        templateResolver.setSuffix(".html");
-        templateResolver.setTemplateMode("HTML");
-        templateResolver.setCharacterEncoding("UTF-8");
-
-        return templateResolver;
-    }*/
-
-    /*@Bean
-    public SpringSecurityDialect springSecurityDialect() {
-        return new SpringSecurityDialect();
-    }*/
-
-    /*@Bean
-    public SpringTemplateEngine templateEngine() {
-        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
-        //templateEngine.addDialect(new Java8TimeDialect());
-        templateEngine.addDialect(new SpringSecurityDialect());
-        return templateEngine;
-    }*/
-
     @Bean
     public SpringTemplateEngine templateEngine() {
-        // SpringTemplateEngine automatically applies SpringStandardDialect and
-        // enables Spring's own MessageSource message resolution mechanisms.
         SpringTemplateEngine templateEngine = new SpringTemplateEngine();
         templateEngine.setTemplateResolver(templateResolver());
         templateEngine.addDialect(new Java8TimeDialect());
@@ -155,10 +106,15 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(final ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/webjars/**").addResourceLocations("/webjars/").resourceChain(false);
-        registry.addResourceHandler("/img/**").addResourceLocations("classpath:/static/img/").resourceChain(false);
-        registry.addResourceHandler("/fonts/**").addResourceLocations("classpath:/static/fonts/").resourceChain(false);
-        registry.addResourceHandler("/css/**").addResourceLocations("classpath:/static/css/").resourceChain(false);
-        registry.addResourceHandler("/js/**").addResourceLocations("classpath:/static/js/").resourceChain(false);
+        registry.addResourceHandler("/webjars/**").addResourceLocations("/webjars/")
+                .resourceChain(false);
+        registry.addResourceHandler("/img/**").addResourceLocations("classpath:/static/img/")
+                .resourceChain(false);
+        registry.addResourceHandler("/fonts/**").addResourceLocations("classpath:/static/fonts/")
+                .resourceChain(false);
+        registry.addResourceHandler("/css/**").addResourceLocations("classpath:/static/css/")
+                .resourceChain(false);
+        registry.addResourceHandler("/js/**").addResourceLocations("classpath:/static/js/")
+                .resourceChain(false);
     }
 }
