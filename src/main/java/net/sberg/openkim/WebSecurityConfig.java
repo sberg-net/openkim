f8af8ef808677d82d84ef7605fc1c6791ee80cd2
stephan <st.schoenberg@sberg.net>
2023-01-12 23:08:16 +0100

    cleanup

--- src/main/java/net/sberg/openkim/WebSecurityConfig.java
+++ src/main/java/net/sberg/openkim/WebSecurityConfig.java
@@ -22,7 +22,6 @@ import net.sberg.openkim.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
